d0511d8b75fd8a37aa2a5eff55e2493349bc6120
mujketa <76135739+mujketa@users.noreply.github.com>
2021-05-24 00:27:35 +0200

    Update PROJEKAT.cpp

--- PROJEKAT.cpp
+++ PROJEKAT.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<iostream>
 #include<cmath>
 #include<algorithm>
 #include<stdlib.h>
@@ -7,116 +8,82 @@
 #include<string>
 #include<vector>
 #include<fstream>   
-#include<sstream> 
+#include<sstream>
 using namespace std;
 
+enum nazivMobitela {Samsung=1, iPhone, Huawei, Xiaomi,};
+
 struct mobitel{
-    string nazivMobitela;
+    nazivMobitela naziv;
     string modelMobitela;
+    int godinaProizvodnje;
+    int RAM;
+    int ROM;
 
 };
-/*
-int unosMobitela() {
+mobitel unosMobitela(){
+    ofstream ispis("bazaMobitela.txt");
+    if(ispis.fail()){
+        cout<<"ERROR";
+    }else{
+        int izbor;
+        do{
+            cout<<"----------------------"<<endl;
+            cout<<"\t1-Samsung"<<endl;
+            cout<<"\t2-iPhone"<<endl;
+            cout<<"\t3-Huawei"<<endl;
+            cout<<"\t4-Xiaomi"<<endl;
+            cout<<"----------------------"<<endl;
+            cout << "Unesite redni broj proizvodjaca mobitela: ";
+            do{
+                cin>>izbor;
+                ispis<<(nazivMobitela)izbor<<" ";
+            }while(izbor<1 || izbor>4);
+            
+            system("cls");
+        }while(1);
+    }
+
+}
+int adminMeni(){
+   	system("cls");
 	int izbor;
-	system("cls");
-	do
-	{
-		cout << crt << "\t::ADMIN::" << crt;
-		cout << "1. Unos naziva mobitela: " << endl;
-		cout << "2. Unos modela mobitela: " << endl;
-		cout << "3. Unos godine proizvodnje: " << endl;
-		cout << "4. RAM: " << endl;
-		cout << "5. Resetuj test za studenta" << endl;		
-		cout << "6. Prikazi sve studente" << endl;
-		cout << "7. Priakzi sva pitanja sa testa" << endl;
-		cout << "8. Kraj rada" << crt;
+	do{
+		cout << "1. Dodati novi artikal(mobitel): " << endl;
+		cout << "2. Provjera stanja artikala: " << endl;
+		cout << "3. Prodaj artikal: " << endl;
 		cout << "Unesite izbor: ";
 		cin >> izbor;
-	} while (izbor<1 || izbor>8);
-	return izbor;
-}
-*/
-void mainmenu();
+		cin.ignore();
+	}while(izbor<1 || izbor>3); 
 
-
-int choice; 
-bool cinfail; 
-int confirmation; 
-string username, password, password2;
-
-void writetofile(string username){
-    ofstream writefile;
-    string file = username+".txt";
-    writefile.open(file.c_str());
-    writefile << password;
-    writefile.close();
-    mainmenu(); }
-
-void login(){
-    cout << "You are being logged in!";}
-
-
-void registerpassword(){
-    cout << "Ukucajte vasu sifru:" << endl;
-    cin >> password;
-    cout << "Ukucajte ponovo vasu sifru:" << endl;
-    cin >> password2;
-    if (password == password2){
-        cin.clear();
-        cin.ignore(10000,'\n');
-        writetofile(username);
-        exit(1);
+    switch(izbor){
+        case 1:
+        system("cls");
+        unosMobitela();
     }
-    else;{
-        cout << "Neispravna sifra" << endl;
-        registerpassword();
-    }}
-
-
-void registerme(){
-    cout << "Molimo ukucajte vase korisnicko ime: " << endl;
-    getline(cin, username);
-    cout << "\nKorisnicko ime -  \""<< username << "\"\nPotvrditi? \n\n[1] Da\n[2] Ne" << endl;
-    cin >> confirmation;
-    if (confirmation == 1){
-        registerpassword();
-    }
-
-    else; {
-        cout << "Sorry invalid input, Please try again" << endl;
-        cin.clear();
-        cin.ignore(10000,'\n');
-        registerme();
-    }}
-
-
-void exit(){
-    exit(0);}
-
-void mainmenu(){ cout << "Hello, Would you like to log in or register\n\t[1] Login\n\t[2] Register\n\t[3] Exit" <<endl; cin >> choice; do{
-    cinfail = cin.fail();
-    cin.clear();
-    cin.ignore(10000,'\n');
-
-    }while(cinfail == true);{
-        switch(choice){
-            case 1:
-                login();
-                break;
-
-            case 2:
-                registerme();
-                break;
-
-            case 3:
-                exit();}}} 
-
-main(){ 
-mainmenu(); 
 }
 
+int loginMeni(){
+	system("cls");
+	int izbor;
+	do{
+        cout<<"\t\tDobrodosli u Mobi-SHOP"<<endl;
+		cout <<"\n:::LOGIN / REGISTER:::"<<endl;
+		cout << "1. Login " << endl;
+		cout << "2. Registracija usera " << endl;
+		cout << "3. Napustiti sistem " << endl;
+		cout << "Unesite izbor: ";
+		cin >> izbor;
+		cin.ignore();
+	}while(izbor<1 || izbor>3);
+
+    if (izbor==1) adminMeni();
+}
+int main (){
+loginMeni();
 
 
 
-
-
+return 0;
+}
