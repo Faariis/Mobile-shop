5decf2b22aff50beb0bb606f243dd12a4aa7cd96
mujketa <76135739+mujketa@users.noreply.github.com>
2021-06-01 04:04:57 +0200

    Update PROJEKAT.cpp

--- PROJEKAT.cpp
+++ PROJEKAT.cpp
@@ -5,85 +5,301 @@
 #include<stdlib.h>
 #include<ctime>
 #include<iomanip>
-#include<string>
+#include<cstring>
 #include<vector>
 #include<fstream>   
 #include<sstream>
+#include <algorithm>
+
 using namespace std;
 
 enum nazivMobitela {Samsung=1, iPhone, Huawei, Xiaomi,};
-
+struct Admin{
+	string username;
+	string sifra;
+};
 struct mobitel{
     nazivMobitela naziv;
     string modelMobitela;
     int godinaProizvodnje;
     int RAM;
     int ROM;
+    int kolicina;
+    float cijena;
 
-};
-mobitel unosMobitela(){
-    ofstream ispis("bazaMobitela.txt");
-    if(ispis.fail()){
-        cout<<"ERROR";
-    }else{
-        int izbor;
-        do{
-            cout<<"----------------------"<<endl;
-            cout<<"\t1-Samsung"<<endl;
-            cout<<"\t2-iPhone"<<endl;
-            cout<<"\t3-Huawei"<<endl;
-            cout<<"\t4-Xiaomi"<<endl;
-            cout<<"----------------------"<<endl;
-            cout << "Unesite redni broj proizvodjaca mobitela: ";
-            do{
-                cin>>izbor;
-                ispis<<(nazivMobitela)izbor<<" ";
-            }while(izbor<1 || izbor>4);
-            
-            system("cls");
-        }while(1);
+	string vratiMob(){
+        switch (naziv){
+            case 1:
+            return "Samsung";
+            case 2:
+            return "iPhone";
+            case 3: 
+            return "Huawei";
+            case 4:
+            return "Xiaomi";
+	    }
     }
+    	string vratiROM(int izbor){
+        switch (izbor){
+            case 1:
+            return "8";
+            case 2:
+            return "16";
+            case 3: 
+            return "32";
+            case 4:
+            return "64";
+            case 5:
+            return "128";
+            case 6:
+            return "256";
+            case 7:
+            return "512";
+            case 8:
+            return "1024";
 
-}
-int adminMeni(){
+	    }
+    }
+    //Login Menu-otvara ga na pocetku programa
+    void loginMeni(){
+	system("cls");
+        cout<<"\t\tDobrodosli u Mobi-SHOP"<<endl;
+		cout <<"\n:::LOGIN / REGISTER:::"<<endl;
+		cout << "1. Registracija usera " << endl;
+		cout << "2. Login " << endl;
+		cout << "0. Napustiti sistem " << endl;
+    }
+    //Admin menu-otvara ga nakon što se prijavimo kao admini
+    void adminMeni(){
    	system("cls");
 	int izbor;
 	do{
 		cout << "1. Dodati novi artikal(mobitel): " << endl;
 		cout << "2. Provjera stanja artikala: " << endl;
 		cout << "3. Prodaj artikal: " << endl;
+        cout <<"0. Kraj: "<<endl;
 		cout << "Unesite izbor: ";
 		cin >> izbor;
 		cin.ignore();
-	}while(izbor<1 || izbor>3); 
+	}while(izbor<0 || izbor>3); 
 
     switch(izbor){
         case 1:
         system("cls");
         unosMobitela();
+        case 2:
+        system("cls");
+        ProvjeraStanjaMeni();
+        case 3:
+        system("cls");
+        case 0:
+        loginMeni();
+
     }
 }
+    //funkcija za unos mobitela u sklopu Admin menua
+    void unosMobitela(){
+    ifstream some("skladiste.txt");
+    string temp;
+    some>>temp;
+    if(some.eof()){
+        some.close();
+        ofstream open("skladiste.txt");
+        open<<"-------------------------------------------------------------------------"<<endl;
+        open<<setw(5)<<"Proizvodjac:"<<setw(10)<<"Model:"<<setw(10)<<"Godina proizvodnje:"<<setw(10)<<"RAM(GB):"<<left<<setw(10)<<"ROM(GB):"<<left<<setw(10)<<"Količina:"<<left<<setw(10)<<"Cijena(KM):"<<endl;
+        open<<"-------------------------------------------------------------------------"<<endl;
+        open.close();
+    }else some.close();
+    
+    ofstream unos("skladiste.txt", ios::app);
+    if(unos.fail()){
+        cout<<"Nemoguce pristupiti bazi podataka!!!";
+    }else{
+        int izbor;
+            cout<<"----------------------"<<endl;
+            cout<<"\t1-Samsung"<<endl;
+            cout<<"\t2-iPhone"<<endl;
+            cout<<"\t3-Huawei"<<endl;
+            cout<<"\t4-Xiaomi"<<endl;
+            cout<<"----------------------"<<endl;
+                do{
+                    cout << "Unesite redni broj proizvodjaca mobitela: ";
+                    cin>>izbor;
+                }while(izbor<1 || izbor>4);
+            naziv=(nazivMobitela)izbor;
+                unos<<setw(5)<<vratiMob();
+                cin.ignore();
+            cout<<"Unesite model mobitela: ";
+                getline(cin, modelMobitela);
+                unos<<" "<<modelMobitela;
+            cout<<"Unesite godinu proizvodnje: ";
+                cin>>godinaProizvodnje;
+                unos<<" "<<godinaProizvodnje;
+            cout<<"Unesite kolicinu RAM memorije (GB): ";
+                cin>>RAM;
+                unos<<" "<<RAM<<"GB";
+                int izbor2;
+                do{
+                    cout<<"Unesite kolicinu ROM memorije (GB): "<<endl;
+                    cout<<"(1)-8 GB, (2)-16 GB, (3)-32 GB, (4)-64 GB, (5)-128 GB, (6)-264 GB, (7)-512 GB, (8)-1024 GB"<<endl;
+                    cin>>izbor2;
+                }while(izbor2<1 || izbor2 >8);    
+                unos<<" "<<vratiROM(izbor2)<<"GB";
+            cout<<"Unesite kolicinu:";
+                cin>>kolicina;
+                unos<<kolicina;
+            cout<<"Unesite cijenu:";
+                cin>>cijena;
+                unos<<cijena;
+                unos<<endl;
+                unos.close();
+                int x;
+                do{
+                    cout<<"\nZelite li unijeti jos jedan mobitel: (0-NE, 1-DA)";
+                    cin>>x;
+                }while(x<0 || x>1);
+                switch(x){
+                    case 0:
+                    adminMeni();
+                    case 1:
+                    system("cls");
+                    unosMobitela();
+                }
+            }
+            unos.close();
+        }
+        //menu koji izbacuje nakon što odaberemo provjera stanja u admin menu
+        void ProvjeraStanjaMeni(){
+            system("cls");
+            int izbor;
+            do{
+                cout << "1. Ispisi sve artikle (sortirano): " << endl;
+                cout << "2. Provjeri mobitel u skladistu : " << endl;
+                cout << "0. Nazad: " << endl;
+                cout << "Unesite izbor: ";
+                cin >> izbor;
+                cin.ignore();
+            }while(izbor<0 || izbor>2);
+            switch (izbor){
+                case 1:
+                system("cls");
+                IspisiSortirano();
+                case 0:
+                adminMeni();
+            } 
+        
+        }
+        //menu koji izbacuje nakon sto izaberemo ispis svih sortiranih artikala
+        void IspisiSortirano(){
+        system("cls");
+        int izbor;
+        do{
+            cout << "1. Sortirano po RAM-u: " << endl;
+            cout << "2. Sortirano po ROM-u: " << endl;
+            cout << "3. Sortirano po cijeni: " << endl;
+            cout << "0. Nazad: " << endl;
+            cout << "Unesite izbor: ";
+            cin >> izbor;
+            cin.ignore();
+        }while(izbor<0 || izbor>3);
 
-int loginMeni(){
+        switch (izbor){
+            case 0:
+            ProvjeraStanjaMeni();
+        } 
+    
+    }
+
+};
+void registracija(string *username,string *pasword,int j){
+	cout<<"Unesite username:";
+    getline(cin,username[j]);
+    cout<<"Unesite pasword:";
+    getline(cin,pasword[j]);
+}
+void login(string *lusername,string *lpasword,int i){
+	cout<<"Username:";
+    getline(cin,lusername[i]);
+    cout<<"Pasword:";
+    getline(cin,lpasword[i]);
+}
+
+bool reg(string *regU,string *regP,string *luser,string *lpas,int i,int j){
+	int k=0;
+	for(int n=0;n<j;n++){
+		if (strcmp(luser[i-1].c_str(),regU[n].c_str()) == 0 && strcmp(lpas[i-1].c_str(),regP[n].c_str())== 0){	
+				k++;
+			}	}
+		if(k==0) return false;
+		else	return true;
+		
+}
+
+
+
+bool adm(string *luser,string *lpas,int i,Admin *novi ){
+	int k=0;
+	for(int n=0;n<4;n++){
+		if (strcmp(luser[i-1].c_str(),novi[n].username.c_str()) == 0 && strcmp(lpas[i-1].c_str(),novi[n].sifra.c_str())== 0){	
+				k++;
+			}	}
+		if(k==0) return false;
+		else	return true;
+}
+
+
+void korisnickiMenu(){
 	system("cls");
-	int izbor;
+	cout<<"-----------------------KORISNICKI MENU!---------------------------------"<<endl;
+}
+
+int main (){
+    mobitel user;
+	int izbor,a=0,b=0,br=0;
+	string username[20],pasword[20],lusername[20],lpasword[20];
+		Admin *novi=new Admin[4];
+	ifstream ob("admini.txt");
+			if(ob.is_open()){
+				while(!ob.eof()){
+					ob>>novi[br].username;
+					ob>>novi[br].sifra;
+					br++;
+				}
+			ob.close();
+		}else{
+			cout<<"Neuspjesno otvaranje datoteke!\n";
+		}
+do{
 	do{
-        cout<<"\t\tDobrodosli u Mobi-SHOP"<<endl;
-		cout <<"\n:::LOGIN / REGISTER:::"<<endl;
-		cout << "1. Login " << endl;
-		cout << "2. Registracija usera " << endl;
-		cout << "3. Napustiti sistem " << endl;
-		cout << "Unesite izbor: ";
+		user.loginMeni();
+		cout << "Izbor: ";
 		cin >> izbor;
 		cin.ignore();
-	}while(izbor<1 || izbor>3);
-
-    if (izbor==1) adminMeni();
-}
-int main (){
-loginMeni();
-
-
+	switch(izbor){
+		case 1:
+			registracija(username,pasword,a);
+			a++;
+			break;	
+		case 2:
+			login(lusername,lpasword,b);
+			b++;
+			if(adm(lusername,lpasword,b,novi)){
+			user.adminMeni();
+				system("pause");
+			}
+			if(reg(username,pasword,lusername,lpasword,b,a)){
+				korisnickiMenu();
+				system("pause");
+			}else{
+				cout<<"Pogresan username ili password \n";
+				system("pause");
+			}
+		case 0:
+                system("cls");
+				break;		
+		}
+	}while(izbor != 5);
+	}while(izbor<1 || izbor>5);
 
 return 0;
 }
